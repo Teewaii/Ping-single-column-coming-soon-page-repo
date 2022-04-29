5af073a420f02cd0d2be4fa53cca6cc4254de8a3
Taiye <taiyeajimati@gmail.com>
2022-04-29 06:00:57 +0400

    button color update

--- apps.js
+++
@@ -1,7 +0,0 @@
-function validateForm() {
-    let inPut = document.forms['emailForm']['email'].value;
-    if (inPut == ' ') {
-        formAlert = document.querySelector('p').innerText = 'please provide a valid email address';
-        return false;
-    }
-}
